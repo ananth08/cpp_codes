3550eaaa1ed88774cb9112d6aad6cf4cd4f069c8
ananthrkn <ananth@iastate.edu>
2017-02-07 14:40:17 -0600

    ATOI and STOI

---
+++ Atoi/cmake-build-debug/CMakeFiles/Atoi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Atoi.dir/main.cpp.o"
+  "Atoi.pdb"
+  "Atoi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Atoi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Atoi/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string name = "12345";
+    int a = stoi(name);
+
+    char b[] = {'1', '2', '3', '4'};
+    int c = atoi(b);
+    cout<<a;
+    cout<<"\n"<<c;
+    return 0;
+}
